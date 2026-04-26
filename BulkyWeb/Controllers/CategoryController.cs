c2b972e7386dffd1abfba22fda77290e6495c12d
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-04-26 13:12:05 +0300

    creat category

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test an invalid value ");
+            }
+            if (ModelState.IsValid) { 
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+    }
+}
