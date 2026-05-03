a36b99a1a31b968f757e59fbbcbedee5eb8d5ba7
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-05-03 14:31:05 +0300

    Handel Create button and active him

--- BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -2,12 +2,12 @@ using BulkyWebRazor_Temp.Data;
 using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BulkyWebRazor_Temp.Pages.Categoris
+namespace BulkyWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-        public List<Category> CategoryList {  get; set; }
+        public List<Category> CategoryList { get; set; }
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
@@ -18,4 +18,3 @@ namespace BulkyWebRazor_Temp.Pages.Categoris
         }
     }
 }
- 
