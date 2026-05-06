eb4b1bc5f561fa0f0c96b1c4a176b9761f60c881
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-05-06 13:59:45 +0300

    Implement Reposiyory Interface

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -5,9 +5,10 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
         T Get(Expression<Func<T, bool>> filter);
