2ead83209fea9c4da0a8cbeaf65452c2e6e5a742
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-05-06 14:15:44 +0300

    Implement Category and Interface Repository

---
+++ Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Upgrade(Category category);
+        void save();
+    }
+}
