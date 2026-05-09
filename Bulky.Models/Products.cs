9453c6ae264a03ea2a7a7817c9d487ba4cf03186
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-05-09 16:55:02 +0300

    Creat model for Product CRUD

---
+++ Bulky.Models/Products.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 100)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 100)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 100)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 100)]
+        public double Price100 { get; set; }
+       
+
+
+    }
+}
