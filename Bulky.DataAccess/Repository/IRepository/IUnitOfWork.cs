7aceb22240a2bff2c84fe707b8d034708c2523e1
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-05-22 22:55:57 +0300

    Company CRUD operations

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace Bulky.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         IProductRepository Product  { get; }
+        ICompanyRepository Company { get; }
 
         void save();
     }
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -1,5 +1,7 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +15,13 @@ namespace Bulky.DataAccess.Repository
         private readonly ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
+        public ICompanyRepository Company { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
         }
         
 
