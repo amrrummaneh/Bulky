4a56036effa5942652fc556e94927f778f358c73
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-06-15 14:23:44 +0300

    Shopping Cart UI

---
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+
+    [Area("customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
